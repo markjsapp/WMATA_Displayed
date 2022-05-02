f546ca7f3139f8b77a5bd2a9f8c0cc656dbda13d
Mark <mark.sapp@interfolio.com>
2022-05-01 20:55:47 -0400

    Update main, extracted the GET call into a method, adding carriage so that the arrival time updates without creating a new print out.

--- main_display.py
+++ main_display.py
@@ -9,44 +9,47 @@ def jprint(obj):
     return json.loads(obj)
 
 
-def main():
-    json_response = jprint(data)
-    # Accessing elements example
-    print('Arrival Station: ' + json_response['Trains'][0]['LocationName'])
-    print('Destination Station: ' + json_response['Trains'][0]['DestinationName'])
-    if(json_response['Trains'][0]['Min'] == 'BRD'):
-        print('Train is Currently Boarding')
-    elif(json_response['Trains'][0]['Min'] == 'ARR'):
-        print('Train is Arriving')
-    else: 
-        print('Arriving in: ' + json_response['Trains'][0]['Min'] + ' minutes')
-
-if __name__ == '__main__':
-# Everything below is boiler plate for the rail prediction endpoint
-# Standard subscription limits a user to 50,000 API calls a day 
-# So we're gonna loop this thing to update every thirty seconds
-    while(True): 
-        # Request headers
-        headers = {
+def get_arrival_info(location_code):
+    # Everything below is boiler plate for the rail prediction endpoint
+    # We pass in the 
+    headers = {
             'api_key': '618d7761e61f43c18c13ce9f47a83b9b',
             }
 
-        params = urllib.parse.urlencode({
-        })
+    params = urllib.parse.urlencode({
+    })
 
-        try:
-            conn = http.client.HTTPSConnection('api.wmata.com')
-            conn.request("GET", "/StationPrediction.svc/json/GetPrediction/K01?%s" % params, "{body}", headers)
-            response = conn.getresponse()
-            data = response.read()
-            #print(data)
-            conn.close()
-        except Exception as e:
-            print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    try:
+        conn = http.client.HTTPSConnection('api.wmata.com')
+        conn.request("GET", f'/StationPrediction.svc/json/GetPrediction/{location_code}?%s' % params, "{body}", headers)
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+        return data
+    except Exception as e:
+        return print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
-        # Now we run our thing 
-        main()
+def main():
+    # I only care about the Court House metro
+    location_code = 'K01'
+    return_data = get_arrival_info(location_code)
+    json_response = jprint(return_data)['Trains'][0]
+    print('\rArrival Station: ' + json_response['LocationName'])
+    print('\rDestination Station: ' + json_response['DestinationName'])
+
+    while(True):
+        return_data = get_arrival_info(location_code)
+        json_response = jprint(return_data)['Trains'][0]
+        if(json_response['Min'] == 'BRD'):
+            print('\rTrain is Currently Boarding                  ', end='')
+        elif(json_response['Min'] == 'ARR'):
+            print('\rTrain is Arriving                  ', end='')
+        else: 
+            print('\rArriving in: ' + json_response['Min'] + ' minutes                  ', end='')
         time.sleep(30)
+
+if __name__ == '__main__':
+    main()
 
 # Relevant Station Codes
 #Virginia Square: K03
